33839591ce33774fee7a2113ab0b351bcd1c0927
JakubRed <jakubredmerski@gmail.com>
2024-01-10 20:50:25 +0100

    first form (Transmitter) complete

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Zad4_RS232
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new RS232());
         }
     }
 }
